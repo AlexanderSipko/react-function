d437184edb84f165d413f16abe445d38aa3eb686
Asipko <avsipko@gmail.com>
2023-03-15 09:53:04 +0500

    add id

---
+++ src/components/ApiDjango.jsx
@@ -0,0 +1,56 @@
+
+import React from "react";
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+
+    componentDidMount() {
+        fetch('https://py-todo-api-avsipko.amvera.io/todo')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.id} - {item.text}, {item.done? 'done': "don't done"}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default MyComponent
