e6fb44f1dd8362a839a0d3cf987e32e4c2cd5559
Asipko <avsipko@gmail.com>
2023-03-18 14:58:55 +0500

    add two function

--- src/components/ApiFlask.jsx
+++ src/components/ApiFlask.jsx
@@ -6,62 +6,94 @@ class ApiFlask extends React.Component {
 
     constructor(props) {
       super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        items: [],
-      };
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: [],
+        };
       this.updateInput = React.createRef('')
     }
 
+    componentDidMount() {
+        this.getList()
+    }
     // create 4 function to manipulate ai data
-  
-    // get https://py-todo-api-avsipko.amvera.io/todo
-    // all list
 
     // get https://py-todo-api-avsipko.amvera.io/todo/<id:int>
     // write
 
-    // post https://py-todo-api-avsipko.amvera.io/todo
-    // add new write
-
     // put https://py-todo-api-avsipko.amvera.io/todo/<id:int>
     // update write
 
-    componentDidMount() {
-        fetch('https://py-todo-api-avsipko.amvera.io/todo')
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
+    // delete https://py-todo-api-avsipko.amvera.io/todo/
+    // delete all
 
   handleButtonClick(inputDate) {
     // получаем кef input для формирования строки
-    const newToDo = {
-      id:this.state.items.length + 1,
-      text:inputDate.value,
-      done:false
+      const newToDo = {
+        id:this.state.items.length + 1,
+        text:inputDate.value,
+        done:false
     }
-    console.log(newToDo)
+    this.addToDo(newToDo)
+    // console.log(JSON.stringify(newToDo))
     inputDate.value = ''
   }
 
+  getList = () => {
+      // get https://py-todo-api-avsipko.amvera.io/todo
+      // all list
+      fetch('https://py-todo-api-avsipko.amvera.io/todo')
+        .then(res => res.json())
+        .then((result) => {
+          this.setState({
+          isLoaded: true,
+          items: result
+        });
+    },
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  )
+  }
+
+  addToDo = (body) => {
+    // post https://py-todo-api-avsipko.amvera.io/todo
+    // add new write
+    fetch("https://py-todo-api-avsipko.amvera.io/todo", {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }).then( (response) => {
+      if (response.status === 201){
+        this.getList()
+      }
+   });
+  }
+
+  deleteToDO = () => {
+    // delete https://py-todo-api-avsipko.amvera.io/todo
+    // this.getList()
+    fetch("https://py-todo-api-avsipko.amvera.io/todo", {
+      method: "delete",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then( (response) => {
+      if (response.status === 200){
+        this.getList()
+      }
+   });
+  }
+
+ 
   render() {
     const { error, isLoaded, items } = this.state;
     if (error) {
@@ -72,6 +104,7 @@ class ApiFlask extends React.Component {
       return (
         <div>
           <h2>TODO add 4 function and components one cards</h2>
+          <p onClick={this.deleteToDO}>delete all list</p>
           <input type="text" ref={this.updateInput} />
           <button onClick={() => this.handleButtonClick(this.updateInput.current)} className="buttonAddTask">add new task</button>
           <List items={items}/>
