3ce429e4b24b0dd8936e6c1ac4ad0f6d56c0af5e
Asipko <avsipko@gmail.com>
2023-03-18 20:27:18 +0500

    add function done

--- src/components/ApiFlask/ListTodoApi.jsx
+++ src/components/ApiFlask/ListTodoApi.jsx
@@ -1,15 +1,21 @@
-
+import React from "react"
 
 function OneToDo(props) {
+    const {id, text, done} = props
+    const [isDone, setDone] = React.useState(done)
 
-    const {id, text, done } = props
+    const handlerClick = () => {
+      setDone(isDone => !isDone)
+    }
 
     return (
       <>
-        { done? (
+        { isDone? (
           <div id={id} className="Done"><i>{text}</i> &#9989;</div>
         ):(
-          <div id={id} className="NotDone"><i>{text}</i> &#128205;</div>
+          <div id={id} className="NotDone"><i>{text}</i>
+            <span onClick={handlerClick} className="StatusToDo">&#128205;</span>
+          </div>
         )}
       </>
     )
@@ -18,13 +24,13 @@ function OneToDo(props) {
 
 function List(props) {
 
-    const {items} = props
+    const {items } = props
 
     return (
         <ul className="UlToDo">
             {items.map(item => (
               <li key={item.id}>
-                  <OneToDo {...item}/>
+                  <OneToDo {...item} />
               </li>
             ))}
           </ul>
