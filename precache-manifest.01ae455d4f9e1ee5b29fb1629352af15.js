f7908d56d7ca667bd4d77f26ee98dd6484d11f7a
Asipko <avsipko@gmail.com>
2023-04-02 17:26:58 +0500

    Updates

---
+++ precache-manifest.01ae455d4f9e1ee5b29fb1629352af15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8433c0a03da74ed505ea",
+    "url": "/react-function/static/css/main.edf75dec.chunk.css"
+  },
+  {
+    "revision": "8433c0a03da74ed505ea",
+    "url": "/react-function/static/js/main.8433c0a0.chunk.js"
+  },
+  {
+    "revision": "4b2b3509a5612ab3f84c",
+    "url": "/react-function/static/js/1.4b2b3509.chunk.js"
+  },
+  {
+    "revision": "04ebe66a06139ccebde5",
+    "url": "/react-function/static/js/runtime~main.04ebe66a.js"
+  },
+  {
+    "revision": "5097b11acecf5ccf60795247a68a7cd5",
+    "url": "/react-function/index.html"
+  }
+];
